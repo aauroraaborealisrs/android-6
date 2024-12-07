f035638b5c8ef41267d1bd21a68e0b2b97a3d0af
aauroraaborealisrs <imaginepeach@mail.ru>
2024-12-07 16:00:29 +0300

    feat: передается 3 элемента и показывается менюшка

--- Lab6/app/src/main/java/com/example/lab6/FragmentNoteDetails.java
+++ Lab6/app/src/main/java/com/example/lab6/FragmentNoteDetails.java
@@ -1,3 +1,46 @@
+//package com.example.lab6;
+//
+//import android.os.Bundle;
+//import android.util.Log;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.TextView;
+//
+//import androidx.annotation.NonNull;
+//import androidx.annotation.Nullable;
+//import androidx.fragment.app.Fragment;
+//
+//public class FragmentNoteDetails extends Fragment {
+//
+//    @Nullable
+//    @Override
+//    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+//        return inflater.inflate(R.layout.fragment_note_details, container, false);
+//    }
+//
+//    @Override
+//    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+//        super.onViewCreated(view, savedInstanceState);
+//
+//        Log.d("FragmentNoteDetails", "Фрагмент создан");
+//        Bundle args = getArguments();
+//        if (args != null) {
+//            Note note = args.getParcelable("note");
+//            if (note != null) {
+//                Log.d("FragmentNoteDetails", "Получены данные: " + note.getTitle());
+//                TextView noteContent = view.findViewById(R.id.noteContent);
+//                noteContent.setText(note.getContent());
+//            } else {
+//                Log.e("FragmentNoteDetails", "Ошибка: Данные не получены");
+//            }
+//        } else {
+//            Log.e("FragmentNoteDetails", "Ошибка: Аргументы не переданы");
+//        }
+//    }
+//
+//}
+
 package com.example.lab6;
 
 import android.os.Bundle;
@@ -5,6 +48,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -24,19 +68,35 @@ public class FragmentNoteDetails extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         Log.d("FragmentNoteDetails", "Фрагмент создан");
+
+        // Получаем переданные данные
         Bundle args = getArguments();
         if (args != null) {
             Note note = args.getParcelable("note");
             if (note != null) {
                 Log.d("FragmentNoteDetails", "Получены данные: " + note.getTitle());
+
+                // Отображаем заголовок
+                TextView noteTitle = view.findViewById(R.id.noteTitle);
+                noteTitle.setText(note.getTitle());
+
+                // Отображаем описание
                 TextView noteContent = view.findViewById(R.id.noteContent);
                 noteContent.setText(note.getContent());
+
+                // Настраиваем чекбокс
+                CheckBox noteCheckbox = view.findViewById(R.id.noteCheckbox);
+                noteCheckbox.setChecked(note.isCompleted());
+                noteCheckbox.setOnCheckedChangeListener((buttonView, isChecked) -> {
+                    // Обновляем состояние заметки
+                    note.setCompleted(isChecked);
+                    Log.d("FragmentNoteDetails", "Заметка обновлена: " + note.getTitle() + ", выполнена: " + isChecked);
+                });
             } else {
-                Log.e("FragmentNoteDetails", "Ошибка: Данные не получены");
+                Log.e("FragmentNoteDetails", "Ошибка: Данные заметки не получены");
             }
         } else {
             Log.e("FragmentNoteDetails", "Ошибка: Аргументы не переданы");
         }
     }
-
 }
--- Lab6/app/src/main/java/com/example/lab6/FragmentNoteList.java
+++ Lab6/app/src/main/java/com/example/lab6/FragmentNoteList.java
@@ -1,60 +1,3 @@
-/*package com.example.lab6;
-
-import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class FragmentNoteList extends Fragment {
-    private List<Note> notes = new ArrayList<>();
-    private NoteAdapter.OnNoteClickListener onNoteClickListener;
-
-    public void setNotes(List<Note> notes) {
-        this.notes = notes;
-    }
-
-    public void setOnNoteClickListener(NoteAdapter.OnNoteClickListener listener) {
-        this.onNoteClickListener = listener;
-    }
-
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_note_list, container, false);
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        RecyclerView recyclerView = view.findViewById(R.id.recyclerView);
-        recyclerView.setLayoutManager(new LinearLayoutManager(requireContext()));
-        recyclerView.setAdapter(new NoteAdapter(
-                notes,
-                note -> {
-                    // Обработка клика по заметке
-                    Log.d("FragmentNoteList", "Clicked on note: " + note.getTitle());
-                },
-                (note, isChecked) -> {
-                    // Обработка клика по чекбоксу
-                    Log.d("FragmentNoteList", "Checkbox clicked: " + note.getTitle() + ", isChecked: " + isChecked);
-                }
-        ));
-
-    }
-}*/
-
 
 package com.example.lab6;
 
@@ -100,4 +43,3 @@ public class FragmentNoteList extends Fragment {
         recyclerView.setAdapter(new NoteAdapter(notes, onNoteClickListener)); // Передаем слушатель
     }
 }
-
